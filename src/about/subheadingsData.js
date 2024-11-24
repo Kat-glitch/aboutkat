0dadacea15b5f7c55bd0a402b04cc9ab29659635
Kat-glitch <katelyn.reddy08@gmail.com>
2024-11-24 12:12:00 +0200

    background and about page

---
+++ src/about/subheadingsData.js
@@ -0,0 +1,61 @@
+const subheadingsData = {
+    1: [
+        {
+            title: "About Me",
+            content: (
+                <>
+                    <p>
+
+                    </p>
+                </>
+            ),
+        },
+    ],
+    2: [
+        {
+            title: "2021-Current Diploma In information Technology",
+            content: (
+                <>
+                    <p>
+
+                    </p>
+                </>
+            ),
+        },
+        {
+            title: "2020 Matric",
+            content: (
+                <>
+                    <p>
+
+                    </p>
+                </>
+            ),
+        },
+    ],
+
+    3: [
+        {
+            title: "Technical Suppourt Agent",
+            content: (
+                <>
+                    <p>
+
+                    </p>
+                </>
+            ),
+        },
+        {
+            title: "Juniour Frontend Developer",
+            content: (
+                <>
+                    <p>
+
+                    </p>
+                </>
+            ),
+        },
+    ],
+};
+
+export default subheadingsData;
---
+++ src/playerstats/PlayerStats.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "../styles/playerStats.css";
+
+const PlayerStats = () => {
+    return(
+        <div id="playerStats">
+            <h1> Katelyn.K.Reddy Level 22 </h1>
+            
+            <div id="playerStats_lines">
+                <span id="playerStats_lines_thick"></span>
+                <span id="playerStats_lines_thin"></span>
+            </div>
+            <h2>Fullstack Developer</h2>
+        </div>
+    );
+}; 
+
+export default PlayerStats;
